45f3135959096087988af97bd9bd7e1df514dbaf
Joshua <peth@live.co.uk>
2015-01-10 12:50:35 +0000

    Created Window
    Using the glfw library I created an OpenGL window, where I will work to
    push stuff on to the screen.

--- main.cpp
+++
@@ -1,3 +0,0 @@
-//To be filled in when the time is right.
-//P.S. Am just testing the github updates
-//Another test.
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+int main( void )
+{
+	int height, width;
+
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Finds the primary monitors screen size
+	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	//Sets the height and width to screen size
+	height = mode->height;
+	 width = mode->width;
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( width/2, height/2, "Joshua's Cube World", NULL, NULL);
+
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	
+	// Light blue background
+	glClearColor(0.6f, 1.0f, 1.0f, 0.0f);
+
+	do{	
+		//Nothing at the moment
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
